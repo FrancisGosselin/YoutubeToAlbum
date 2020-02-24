f9b9f276021d561197016011e40a52b0a5af30ca
FrancisGosselin <francis.gosselin@polymtl.ca>
2020-02-25 00:28:37 +0100

    Easier command line

--- albummaker.py
+++ albummaker.py
@@ -6,68 +6,94 @@ import pafy
 import sys
 import numpy as np
 
-plurl = "https://www.youtube.com/playlist?list=PLoXd1MHJHuY6rw5jrlUOt83lDPfy5rgcM"
-playlist = pafy.get_playlist(plurl)
 
 parser = argparse.ArgumentParser()
-parser.add_argument('-s', '--skip-download', action='store_true', dest='skip', help='if you already have the mp3')
+parser.add_argument('url', help="Youtube URL of the playlist")
+
 parser.add_argument('--name', action='store', dest='name', help='name of the album')
 parser.add_argument('--year', action='store', dest='year', help='year of the album')
 parser.add_argument('--genre', action='store', dest='genre', help='genre of the album')
 parser.add_argument('--artist', action='store', dest='artist', help='artist')
 
-parser.add_argument('directory', metavar='d', nargs='+',
-                    help='directory of album')
 args = parser.parse_args()
 
-if not args.skip:
-    print("Collecting playlist information ...")
-    streams = [ item["pafy"].getbestaudio() for item in playlist['items'] ]
-    print("Downloading " + str(len(streams)) + " music files ... Press any key to continue")
-    for stream in streams:
-        filename = stream.title + '.' + stream.extension
+playlist = pafy.get_playlist(args.url)
+
+print("Collecting playlist information ...")
+streams = [ item["pafy"].getbestaudio() for item in playlist['items'] ]
+print("Downloading " + str(len(streams)) + " music files ...")
+
+old_files = []
+files = []
+
+for stream in streams:
+    filename = stream.title + '.' + stream.extension
+
+    filepath =  os.path.join(Path.cwd(), filename)
+    new_filepath = os.path.join(Path.cwd(), stream.title + '.mp3')
+
+    files.append(new_filepath)
+    old_files.append(filepath)
+
+    if os.path.isfile(new_filepath):
+        os.remove(new_filepath)
+
+    child_pid = os.fork()
+    if child_pid == 0:
         try :
-            stream.download(os.path.join(Path.cwd(),args.directory[0]))
+            stream.download(Path.cwd())
         except OSError:
-            stream.download(os.path.join(Path.cwd(),args.directory[0]))
+            stream.download(Path.cwd())
 
-        filepath =  os.path.join(os.path.join(Path.cwd(),args.directory[0]), filename)
-        new_filepath = os.path.join(os.path.join(Path.cwd(),args.directory[0]), stream.title + '.mp3')
+        os.execl('/usr/bin/ffmpeg', '/usr/bin/ffmpeg', '-nostats', '-loglevel', '0', '-i', filepath,"-acodec", "libmp3lame", new_filepath)
+    os.wait()
 
-        child_pid = os.fork()
-        if child_pid == 0:
-            print("started conversion to FLAC")
-            os.execl('/usr/bin/ffmpeg', '/usr/bin/ffmpeg', '-nostats', '-loglevel', '0', '-i', filepath,"-acodec", "libmp3lame", new_filepath)
-        os.wait()
-        os.remove(filepath) 
+for file in old_files:
+    os.remove(file) 
 
-    print("All downloads are done!")
-    print("Continue ? [Y/n]")
-    x = input()
 
-    if x != "y" and x != "Y":
-        print("exiting")
-        sys.exit(0)
-else :
-    print("Skipping download...")
+print("All downloads are done!")
 
+print("\n")
+
+print("Enter the following informations (or leave blank):")
+if not args.name:
+    args.name = input("Album title: ")
+if not args.year:
+    args.year = input("Year: ")
+if not args.genre:
+    args.genre = input("Genre: ")
+if not args.artist:
+    args.artist = input("Artist: ")
+
+print("Correct manualy the names of the songs (or leave blank to leave unchange):")
 num = 1
-for file in os.listdir(os.path.join(Path.cwd(),args.directory[0])):
+for file in files:
     if file.endswith('.mp3'):
-        filepath = os.path.join(os.path.join(Path.cwd(),args.directory[0]), file)
-        audiofile = eyed3.load(filepath)
+
+        path = '/'.join(file.split('/')[:-1])
+        filename = '.'.join(file.split('/')[-1].split('.')[:-1])
+
+        new_filename = input(filename + " --> ")
+        new_file =  path + '/' + new_filename + ".mp3"
+        os.rename(file, new_file)
+
+        audiofile = eyed3.load(new_file)
         audiofile.tag.track_num = num
         num += 1
-        audiofile.tag.title = '.'.join(file.split('.')[:-1])
+        audiofile.tag.title = new_filename
 
-        if args.name:
+        if args.name and args.name != "":
             audiofile.tag.album = args.name
-        if args.year:
+        if args.year and args.year != "":
             audiofile.tag.year = args.year
-        if args.genre:
+        if args.genre and args.genre != "":
             audiofile.tag.genre = args.genre
-        if args.artist:
+        if args.artist and args.artist != "":
             audiofile.tag.album_artist = args.artist
             audiofile.tag.artist = args.artist
         audiofile.tag.save()
 
+        print(new_file)
+
+print("All done!")
